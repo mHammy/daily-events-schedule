ad4c7fb17ab3eb0326b4f095f7c371c449cb1e37
mHammy <133612424+mHammy@users.noreply.github.com>
2023-06-30 17:34:11 -0400

    initial commit with the index.html, style.css, script.js and a fresh README.md

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
+var currentDate = dayjs().format('dddd, MMMM D, YYYY h:mm A');
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
